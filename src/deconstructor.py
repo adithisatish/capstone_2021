e6e5b7f6016ec72d1df33c6d148e3ddfaae65349
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2021-09-10 21:21:45 +0530

    updated spo to handle only verb identification

--- src/deconstructor.py
+++ src/deconstructor.py
@@ -1,7 +1,8 @@
 from app.alliteration.AlliterationDetector import Alliteration
+from app.simile.SimileDetector import Similes
 from app.spo.SPODetector import SPO 
 from app.simile.SimileDetector import detect_similes
 from app.tone.ToneAnalyzer import display_tones, detect_tone
 from app.tense.TenseDetector import tenseDetection
 
-mapComponent = {"SPO": SPO, "Alliteration": Alliteration} # Others yet to be mapped
+mapComponent = {"SPO": SPO, "Alliteration": Alliteration, "Simile": Similes} # Others yet to be mapped
