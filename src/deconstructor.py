b2c9c0bb57723229695d5b52b0692716799180e8
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2021-09-11 22:02:11 +0530

    renamed metaphor detector file

--- src/deconstructor.py
+++ src/deconstructor.py
@@ -1,7 +1,7 @@
 from app.alliteration.AlliterationDetector import Alliteration
 from app.simile.SimileDetector import Similes
 from app.spo.SPODetector import SPO 
-from app.metaphor.MetaphorDetection import Metaphor
+from app.metaphor.MetaphorDetector import Metaphor
 from app.simile.SimileDetector import detect_similes
 from app.tone.ToneAnalyzer import display_tones, detect_tone
 from app.tense.TenseDetector import tenseDetection
