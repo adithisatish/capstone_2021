eb30e58e27f0fd516f61371458286cf9e8715c28
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2021-09-18 17:52:37 +0530

    changed node_env

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const isProduction = process.env.NODE_ENV !=="production"
+const isProduction = process.env.NODE_ENV ==="production"
 export const authServer = axios.create({
     baseURL: isProduction? 'https://sentence-deconstructor.herokuapp.com': 'http://localhost:8080'
 });
