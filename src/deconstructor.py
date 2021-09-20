3f505e2011179a54eabe4bce23f5f61f0645ac05
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2021-09-20 23:42:49 +0530

    mapped voice_tense to deconstructor.py

--- src/deconstructor.py
+++ src/deconstructor.py
@@ -3,7 +3,7 @@ from app.simile.SimileDetector import Similes
 from app.spo.SPODetector import SPO 
 from app.metaphor.MetaphorDetector import Metaphor
 from app.tense.TenseDetector import Tenses
-from app.voice.voice_spo import Voice_Spo
+from app.voice.voice_tense import Voice
 from app.rhyme_scheme.RhymeSchemeDetector import RhymeScheme
 
-mapComponent = {"SPO": SPO, "Alliteration": Alliteration, "Similes": Similes, "Metaphor": Metaphor, "Tense":Tenses, "Voice": Voice_Spo, "Rhyme Scheme":RhymeScheme} # Others yet to be mapped
+mapComponent = {"SPO": SPO, "Alliteration": Alliteration, "Similes": Similes, "Metaphor": Metaphor, "Tense":Tenses, "Voice": Voice, "Rhyme Scheme":RhymeScheme} # Others yet to be mapped
