86430df99f43bcd3c2bdda5492f105cf457e5550
vidishraj <66262548+vidishraj@users.noreply.github.com>
2021-12-08 12:38:02 +0530

    Added Explanation

--- src/app/simile/SimileDetector.py
+++ src/app/simile/SimileDetector.py
@@ -24,12 +24,12 @@ class Similes:
             self.similes[index]['Sentence']=self.text[list_index]
             self.text[list_index] = re.sub('[^a-zA-Z0-9 \n\.]', '', self.text[list_index])
             self.similes[index]['Simile']=list()
+            self.similes[index]['Explanation']=list()
             flag=0
             val=0
             tokenized_text=word_tokenize(self.text[list_index])
             final=nltk.pos_tag(tokenized_text)
             words = self.text[list_index].split()
-
         # For "like" occuring in the sentense
             for x in range(len(words)):
                 if(words[x]in ['like','Like']):
@@ -71,19 +71,73 @@ class Similes:
                                     self.similes[index]['Simile'].append(' '.join(tokenized_text[j:j+4]))
                                     val=1
             index+=1
-        return self.similes
+        #return self.similes
     
+    def helper(self,sent, simile):
+        result=[]
+        words=simile.split()
+        result1=[]
+        tokenized_text=word_tokenize(sent)
+        final=nltk.pos_tag(tokenized_text)
+        for i in range(len(final)):
+            if(final[i][1]!='DT' and final[i][0] not in ["like", "as"]):
+                result.append(final[i])
+        for i in range(len(result)):
+            if(result[i][0] in words):
+                result1.append(result[i])
+        if("as" in simile):
+            result1.append(0)
+        if("like" in simile):
+            result1.append(1)
+        return result1
+    
+    def explanation(self):
+        for k in range(len(self.similes)):
+            if( len(self.similes[k]['Simile'])!=0):
+                Sentence=self.similes[k]['Sentence']
+                for j in range(len(self.similes[k]['Simile'])):
+                    s=re.sub('[^a-zA-Z0-9 ]', '', self.similes[k]['Simile'][j])
+                    ans=self.helper(Sentence,s)
+                    for i in range(0,2):
+                        ans[i]=list(ans[i]) #convert tuple to list
+                    for i in range(0, 2):
+                        if(ans[i][1] in ['VB','VBD','VBG','VBN','VBP','VBZ']):
+                            ans[i][1]=" verb "
+                        elif(ans[i][1] in ["NN","NNS","NNP"]):
+                            ans[i][1]=" noun "
+                        elif(ans[i][1]=="JJ"):
+                            ans[i][1]=" adjective "
+                        elif(ans[i][1]=="RB"):
+                            ans[i][1]=" adverb "
+                        elif(ans[i][1]=="PRP"):
+                            ans[i][1]=" pronoun "
+                    t1=ans[0][1]
+                    w1=ans[0][0]
+                    t2=ans[1][1]
+                    w2=ans[1][0]
+                    x="like"
+                    if(ans[2]==0):
+                        x="as"
+                    Explanation="The sentence \"" + Sentence + "\" is a simile because it compares the"+t1+"\""+w1+"\""+" and the"+t2+"\""+w2+"\""+ " using the word "+"\""+x+"\""
+                    self.similes[k]['Explanation'].append(Explanation)
+            
+
+
     def display(self):
 
         for i in range(len(self.similes)):
             print("Sentence:", self.similes[i]['Sentence'])
             print("Similes:",self.similes[i]['Simile'])
+            print("Explanation:",self.similes[i]['Explanation'])
             print("**************************")
             
     
     def execute(self):
         # Driver function
-        return self.detect_similes()
+        self.detect_similes()
+        self.explanation()
+        #self.display()
+        return self.similes
 
 if __name__ == "__main__":
     text = ["Rob was never as honest as Emily.","He paints like a rainbow in the sky.", "She is as pretty as a flock of birds.","This path meanders like a stream.",
@@ -100,4 +154,3 @@ if __name__ == "__main__":
     
     sim_obj = Similes(text)
     s1=sim_obj.execute()
-    s1=sim_obj.display()
